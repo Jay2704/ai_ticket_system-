cb373fe881cccc066711babc28bfb88856c6619a
Jay2704 <decoder.2704@gmail.com>
2026-06-18 19:39:30 -0400

    ticket.py initiation

--- ai_ticket_system/ticket.py
+++ ai_ticket_system/ticket.py
@@ -1,1 +1,18 @@
 
+class Ticket:
+    
+    def __init__(self):
+        
+        ticket_id = 0
+        user_name = ""
+        issue_description = "" 
+        issue_category = "" 
+        priority = ""
+        status = ""
+        assigned_team = ""
+        created_at = ""
+
+    def create_ticket():
+        pass
+    
+    
