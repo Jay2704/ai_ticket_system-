ddcf6fe794b94dfb71dd3018db0ea107deeeff9f
Jay2704 <decoder.2704@gmail.com>
2026-07-01 19:49:42 -0400

    USers.py basic oops

---
+++ ai_ticket_system/users.py
@@ -0,0 +1,22 @@
+
+class Users:
+
+    def __init__(self, id, name, email, role, dept) -> None:
+        self.user_id = id
+        self.user_name = name
+        self.user_email = email
+        self.role = role
+        self.department = dept
+
+    def display_user(self):
+        pass
+
+    def update_department(self, dept):
+        self.department = dept
+
+    def update_role(self, new_role):
+        self.role = new_role
+
+    def update_emial(self, new_email):
+        self.email = new_email
+    
