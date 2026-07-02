cc89a00c9af74a13a76a5f09d61e5b2c0e0aea9a
Jay2704 <decoder.2704@gmail.com>
2026-07-02 18:20:52 -0400

    users.py file

--- ai_ticket_system/users.py
+++ ai_ticket_system/users.py
@@ -1,27 +1,24 @@
+class User:
 
-class Users:
-
-    def __init__(self, id, name, email, role, dept) -> None:
-        self.user_id = id
+    def __init__(self, user_id, name, email, role, department):
+        self.user_id = user_id
         self.user_name = name
         self.user_email = email
         self.role = role
-        self.department = dept
+        self.department = department
 
     def display_user(self):
-        print(f"User ID         : {self.user_id}")
-        print(f"User Name       : {self.user_name}")
-        print(f"Email           : {self.user_email}")
-        print(f"Role            : {self.role}")
-        print(f"Department      : {self.department}")
+        print(f"User ID    : {self.user_id}")
+        print(f"User Name  : {self.user_name}")
+        print(f"Email      : {self.user_email}")
+        print(f"Role       : {self.role}")
+        print(f"Department : {self.department}")
 
-    def update_department(self, dept):
-        self.department = dept
+    def update_department(self, department):
+        self.department = department
 
     def update_role(self, new_role):
         self.role = new_role
 
     def update_email(self, new_email):
-        self.email = new_email
-    
-    
+        self.user_email = new_email
