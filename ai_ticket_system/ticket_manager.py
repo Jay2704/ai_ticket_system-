6ebbd2ecc95ae0d93e947764d1ef7c7e7d385b1a
Jay2704 <decoder.2704@gmail.com>
2026-07-02 06:40:55 -0400

    bug removed in ticket manager py file:

--- ai_ticket_system/ticket_manager.py
+++ ai_ticket_system/ticket_manager.py
@@ -8,4 +8,25 @@ class TicketManager:
     def get_all_tickets(self):
         return self.tickets
 
-    
+    def remove_ticket(self, ticket):
+        self.tickets.remove(ticket)
+
+    def find_ticket(self, ticket_id):
+        for ticket in self.tickets:
+            if ticket.ticket_id == ticket_id:
+                return ticket
+        return None
+
+    def update_status(self, ticket_id, new_status):
+        ticket = self.find_ticket(ticket_id)
+        if ticket:
+            ticket.update_status(new_status)
+
+    def update_priority(self, ticket_id, new_priority):
+        ticket = self.find_ticket(ticket_id)
+        if ticket:
+            ticket.update_priority(new_priority)
+
+    def show_all_tickets(self):
+        for ticket in self.tickets:
+            ticket.get_ticket_summary()
