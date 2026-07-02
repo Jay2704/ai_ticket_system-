579a8f14c818739ecb35515c566a3edd83e076e3
Jay2704 <decoder.2704@gmail.com>
2026-07-02 06:49:21 -0400

    main file done

--- ai_ticket_system/main.py
+++ ai_ticket_system/main.py
@@ -1,1 +1,13 @@
+from users import Users
+from ticket import Ticket
+from ticket_manager import TicketManager
+
+user1 = Users(1, "Jay", "jay@gmail.com", "1234567890")
+user2 = Users(2, "John", "john@gmail.com", "1234567890")
+user3 = Users(3, "Jane", "jane@gmail.com", "1234567890")
+user4 = Users(4, "Jim", "jim@gmail.com", "1234567890")
+user5 = Users(5, "Jill", "jill@gmail.com", "1234567890")
+
+
+
 
